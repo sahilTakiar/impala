96730b517bf46740716b5822b32340f8226703c2
Sahil Takiar <takiar.sahil@gmail.com>
2019-07-26 11:52:01 -0700

    IMPALA-8803: Coordinator should release admitted memory per-backend
    Changes the Coordinator to release admitted memory when each Backend
    completes, rather than waiting for the entire query to complete before
    releasing admitted memory. When the Coordinator detects that a Backend
    has completed (via ControlService::ReportExecStatus) it updates the
    state of the Backend in Coordinator::BackendResourceState.
    BackendResourceState tracks the state of the admitted resources for
    each Backend and decides when the resources for a group of Backend
    states should be released. BackendResourceState defines a state machine
    to help coordinate the state of the admitted memory for each Backend.
    It guarantees that by the time the query is shutdown, all Backends
    release their admitted memory.
    
    BackendResourceState implements three rules to control the rate at
    which the Coordinator releases admitted memory from the
    AdmissionController:
    * Resources are released at most once every 1 second, this prevents
    short lived queries from causing high load on the admission controller
    * Resources are released at most O(log(num_backends)) times; the
    BackendResourceStates can release multiple BackendStates from the
    AdmissionController at a time
    * All pending resources are released if the only remaining Backend is
    the Coordinator Backend; this is useful for result spooling where all
    Backends may complete, except for the Coordinator Backend
    
    Testing:
    * Ran core tests
    * Added new tests to test_result_spooling.py and
    test_admission_controller.py
    
    Change-Id: I88bb11e0ede7574568020e0277dd8ac8d2586dc9

--- be/src/runtime/coordinator-backend-state.cc
+++ be/src/runtime/coordinator-backend-state.cc
@@ -58,17 +58,18 @@ const char* Coordinator::BackendState::InstanceStats::LAST_REPORT_TIME_DESC =
 DECLARE_int32(backend_client_rpc_timeout_ms);
 DECLARE_int64(rpc_max_message_size);
 
-Coordinator::BackendState::BackendState(
-    const Coordinator& coord, int state_idx, TRuntimeFilterMode::type filter_mode)
-  : coord_(coord),
+Coordinator::BackendState::BackendState(const QuerySchedule& schedule,
+    const TQueryCtx& query_ctx, int state_idx,
+    TRuntimeFilterMode::type filter_mode, const BackendExecParams& exec_params)
+  : schedule_(schedule),
     state_idx_(state_idx),
-    filter_mode_(filter_mode) {
-}
+    filter_mode_(filter_mode),
+    backend_exec_params_(&exec_params),
+    query_ctx_(query_ctx),
+    query_id_(schedule.query_id()) {}
 
-void Coordinator::BackendState::Init(
-    const BackendExecParams& exec_params, const vector<FragmentStats*>& fragment_stats,
+void Coordinator::BackendState::Init(const vector<FragmentStats*>& fragment_stats,
     RuntimeProfile* host_profile_parent, ObjectPool* obj_pool) {
-  backend_exec_params_ = &exec_params;
   host_ = backend_exec_params_->be_desc.address;
   krpc_host_ = backend_exec_params_->be_desc.krpc_address;
   num_remaining_instances_ = backend_exec_params_->instance_params.size();
@@ -105,7 +106,7 @@ void Coordinator::BackendState::SetRpcParams(const DebugOptions& debug_options,
   request->set_min_mem_reservation_bytes(backend_exec_params_->min_mem_reservation_bytes);
   request->set_initial_mem_reservation_total_claims(
       backend_exec_params_->initial_mem_reservation_total_claims);
-  request->set_per_backend_mem_limit(coord_.schedule_.per_backend_mem_limit());
+  request->set_per_backend_mem_limit(schedule_.per_backend_mem_limit());
 
   // set fragment_ctxs and fragment_instance_ctxs
   sidecar->__isset.fragment_ctxs = true;
@@ -173,7 +174,7 @@ void Coordinator::BackendState::SetRpcParams(const DebugOptions& debug_options,
 void Coordinator::BackendState::SetExecError(const Status& status) {
   const string ERR_TEMPLATE = "ExecQueryFInstances rpc query_id=$0 failed: $1";
   const string& err_msg =
-      Substitute(ERR_TEMPLATE, PrintId(query_id()), status.msg().GetFullMessageDetails());
+      Substitute(ERR_TEMPLATE, PrintId(query_id_), status.msg().GetFullMessageDetails());
   LOG(ERROR) << err_msg;
   status_ = Status::Expected(err_msg);
 }
@@ -204,7 +205,7 @@ void Coordinator::BackendState::Exec(
 
   ExecQueryFInstancesRequestPB request;
   TExecQueryFInstancesSidecar sidecar;
-  sidecar.__set_query_ctx(query_ctx());
+  sidecar.__set_query_ctx(query_ctx_);
   SetRpcParams(debug_options, filter_routing_table, &request, &sidecar);
 
   RpcController rpc_controller;
@@ -242,7 +243,7 @@ void Coordinator::BackendState::Exec(
 
   VLOG_FILE << "making rpc: ExecQueryFInstances"
       << " host=" << TNetworkAddressToString(impalad_address()) << " query_id="
-      << PrintId(query_id());
+      << PrintId(query_id_);
 
   // guard against concurrent UpdateBackendExecStatus() that may arrive after RPC returns
   lock_guard<mutex> l(lock_);
@@ -268,7 +269,7 @@ void Coordinator::BackendState::Exec(
   }
 
   for (const auto& entry: instance_stats_map_) entry.second->stopwatch_.Start();
-  VLOG_FILE << "rpc succeeded: ExecQueryFInstances query_id=" << PrintId(query_id());
+  VLOG_FILE << "rpc succeeded: ExecQueryFInstances query_id=" << PrintId(query_id_);
 }
 
 Status Coordinator::BackendState::GetStatus(bool* is_fragment_failure,
@@ -334,7 +335,7 @@ void Coordinator::BackendState::LogFirstInProgress(
     std::vector<Coordinator::BackendState*> backend_states) {
   for (Coordinator::BackendState* backend_state : backend_states) {
     if (!backend_state->IsDone()) {
-      VLOG_QUERY << "query_id=" << PrintId(backend_state->query_id())
+      VLOG_QUERY << "query_id=" << PrintId(backend_state->query_id_)
                  << ": first in-progress backend: "
                  << TNetworkAddressToString(backend_state->impalad_address());
       break;
@@ -500,21 +501,21 @@ bool Coordinator::BackendState::Cancel() {
   // set an error status to make sure we only cancel this once
   if (status_.ok()) status_ = Status::CANCELLED;
 
-  VLOG_QUERY << "Sending CancelQueryFInstances rpc for query_id=" << PrintId(query_id())
+  VLOG_QUERY << "Sending CancelQueryFInstances rpc for query_id=" << PrintId(query_id_)
              << " backend=" << TNetworkAddressToString(krpc_host_);
 
   std::unique_ptr<ControlServiceProxy> proxy;
   Status get_proxy_status = ControlService::GetProxy(krpc_host_, host_.hostname, &proxy);
   if (!get_proxy_status.ok()) {
     status_.MergeStatus(get_proxy_status);
-    VLOG_QUERY << "Cancel query_id= " << PrintId(query_id()) << " could not get proxy to "
+    VLOG_QUERY << "Cancel query_id= " << PrintId(query_id_) << " could not get proxy to "
                << TNetworkAddressToString(krpc_host_)
                << " failure: " << get_proxy_status.msg().msg();
     return true;
   }
 
   CancelQueryFInstancesRequestPB request;
-  TUniqueIdToUniqueIdPB(query_id(), request.mutable_query_id());
+  TUniqueIdToUniqueIdPB(query_id_, request.mutable_query_id());
   CancelQueryFInstancesResponsePB response;
 
   const int num_retries = 3;
@@ -522,12 +523,12 @@ bool Coordinator::BackendState::Cancel() {
   const int64_t backoff_time_ms = 3 * MILLIS_PER_SEC;
   Status rpc_status =
       RpcMgr::DoRpcWithRetry(proxy, &ControlServiceProxy::CancelQueryFInstances, request,
-          &response, query_ctx(), "Cancel() RPC failed", num_retries, timeout_ms,
+          &response, query_ctx_, "Cancel() RPC failed", num_retries, timeout_ms,
           backoff_time_ms, "COORD_CANCEL_QUERY_FINSTANCES_RPC");
 
   if (!rpc_status.ok()) {
     status_.MergeStatus(rpc_status);
-    VLOG_QUERY << "Cancel query_id= " << PrintId(query_id()) << " could not do rpc to "
+    VLOG_QUERY << "Cancel query_id= " << PrintId(query_id_) << " could not do rpc to "
                << TNetworkAddressToString(krpc_host_)
                << " failure: " << rpc_status.msg().msg();
     return true;
@@ -535,7 +536,7 @@ bool Coordinator::BackendState::Cancel() {
   Status cancel_status = Status(response.status());
   if (!cancel_status.ok()) {
     status_.MergeStatus(cancel_status);
-    VLOG_QUERY << "Cancel query_id= " << PrintId(query_id())
+    VLOG_QUERY << "Cancel query_id= " << PrintId(query_id_)
                << " got failure after rpc to " << TNetworkAddressToString(krpc_host_)
                << " failure: " << cancel_status.msg().msg();
     return true;
@@ -544,7 +545,7 @@ bool Coordinator::BackendState::Cancel() {
 }
 
 void Coordinator::BackendState::PublishFilter(const TPublishFilterParams& rpc_params) {
-  DCHECK(rpc_params.dst_query_id == query_id());
+  DCHECK(rpc_params.dst_query_id == query_id_);
   // If the backend is already done, it's not waiting for this filter, so we skip
   // sending it in this case.
   if (IsDone()) return;
@@ -784,4 +785,125 @@ void Coordinator::BackendState::InstanceStatsToJson(Value* value, Document* docu
   // protected by Coordinator::lock_.
   Value val(TNetworkAddressToString(impalad_address()).c_str(), document->GetAllocator());
   value->AddMember("host", val, document->GetAllocator());
+}
+
+DEFINE_uint32_hidden(batched_release_decay_factor, 2,
+    "The exponential decay factor for the 'Batched Release' of backends. The default of "
+    "2 ensures that the number of times resources are released is bounded by O(log2(n)). "
+    "Setting this to another value, such as 10, would bound the number of times "
+    "resources are released by O(log10(n)).");
+DEFINE_uint64_hidden(release_backend_states_delay_ms, 1000,
+    "The timeout for the 'Timed Release' of backends. If more than this many "
+    "milliseconds has passed since the last time any Backends were released, then "
+    "release all pending Backends. Set to 1000 milliseconds by default.");
+
+DECLARE_uint32(batched_release_decay_factor);
+DECLARE_uint64(release_backend_states_delay_ms);
+
+Coordinator::BackendResourceState::BackendResourceState(
+    const vector<BackendState*>& backend_states, const QuerySchedule& schedule)
+  : backend_states_(backend_states),
+    num_backends_(backend_states.size()),
+    schedule_(schedule),
+    release_backend_states_delay_ns_(FLAGS_release_backend_states_delay_ms * 1000000),
+    batched_release_decay_value_(FLAGS_batched_release_decay_factor) {
+  DCHECK_GT(batched_release_decay_value_, 0)
+      << "Invalid value for --batched_release_decay_factor, must greater than 0";
+  // Populate the backend_resource_states_ map and mark all BackendStates as
+  // IN_USE.
+  for (auto backend_state : backend_states_) {
+    backend_resource_states_[backend_state] = ResourceState::IN_USE;
+  }
+  // Start the 'Timed Release' timer.
+  released_timer_.Start();
+}
+
+Coordinator::BackendResourceState::~BackendResourceState() {
+  // Assert that all BackendStates have been released. We use num_backends_ instead of
+  // backend_states_.size() so backend_states_ does not need to be alive when the
+  // destructor runs.
+  DCHECK(closed_);
+  DCHECK_EQ(num_released_, num_backends_);
+}
+
+void Coordinator::BackendResourceState::MarkBackendFinished(
+    BackendState* backend_state, vector<BackendState*>* releasable_backend_states) {
+  lock_guard<SpinLock> lock(lock_);
+  if (!closed_
+      && backend_resource_states_.at(backend_state) == ResourceState::IN_USE) {
+    // Transition the BackendState to PENDING and update any related counters.
+    backend_resource_states_.at(backend_state) = ResourceState::PENDING;
+    ++num_pending_;
+    ++num_pending_releasable_or_released_;
+
+    // If the coordinator backend has not been released, but all other have, then the only
+    // running backend must be the coordinator. The coordinator fragment should buffer
+    // enough rows to allow all other fragments to be released (if result spooling is
+    // enabled this is especially true, but even without spooling many queries have a
+    // coordinator fragment that buffers multiple RowBatches). If the client does not
+    // fetch all rows immediately, then the Coordinator Backend will be long lived
+    // compared to the rest of the Backends.
+    bool is_coordinator_the_last_unreleased_backend = !released_coordinator_
+        && num_pending_releasable_or_released_ == num_backends_ - 1;
+
+    // True if the 'Timed Release' heuristic should be triggered.
+    bool release_backends_timeout_expired =
+        released_timer_.ElapsedTime() > release_backend_states_delay_ns_;
+
+    // True if the 'Batched Release' heuristic should be triggered.
+    bool unreleased_backend_threshold_reached = num_pending_
+        >= std::max(floor(num_backends_ / batched_release_decay_value_), 1.0);
+
+    // If no backends are running or if only the coordinator backend is running or if both
+    // the 'Timed Release' and 'Batched Release' heuristic are true, then transition all
+    // PENDING backend states to releasable and update any state necessary for the
+    // heuristics.
+    if (is_coordinator_the_last_unreleased_backend
+        || (release_backends_timeout_expired && unreleased_backend_threshold_reached)) {
+      released_timer_.Reset();
+      batched_release_decay_value_ *= FLAGS_batched_release_decay_factor;
+      for (auto backend_resource_state : backend_resource_states_) {
+        if (backend_resource_state.second == ResourceState::PENDING) {
+          releasable_backend_states->push_back(backend_resource_state.first);
+          backend_resource_states_[backend_resource_state.first] =
+              ResourceState::RELEASABLE;
+          --num_pending_;
+        }
+      }
+    }
+  }
+}
+
+void Coordinator::BackendResourceState::BackendsReleased(
+    const vector<BackendState*>& released_backend_states) {
+  lock_guard<SpinLock> lock(lock_);
+  // Mark all given BackendStates as RELEASED. A BackendState must be either RELEASABLE
+  // or IN_USE before it can marked as RELEASED.
+  for (auto backend_state : released_backend_states) {
+    DCHECK(backend_resource_states_.at(backend_state) != ResourceState::RELEASED);
+    if (backend_resource_states_.at(backend_state) == ResourceState::IN_USE) {
+      ++num_pending_releasable_or_released_;
+    }
+    backend_resource_states_.at(backend_state) = ResourceState::RELEASED;
+    // If the backend running the coordinator has completed and been released, set
+    // released_coordinator_ to true.
+    if (backend_state->exec_params()->is_coord_backend) {
+      released_coordinator_ = true;
+    }
+  }
+  num_released_ += released_backend_states.size();
+}
+
+vector<Coordinator::BackendState*>
+Coordinator::BackendResourceState::CloseAndGetUnreleasedBackends() {
+  lock_guard<SpinLock> lock(lock_);
+  vector<BackendState*> unreleased_backend_states;
+  for (auto backend_resource_state : backend_resource_states_) {
+    if (backend_resource_state.second == ResourceState::IN_USE
+        || backend_resource_state.second == ResourceState::PENDING) {
+      unreleased_backend_states.push_back(backend_resource_state.first);
+    }
+  }
+  closed_ = true;
+  return unreleased_backend_states;
 }
